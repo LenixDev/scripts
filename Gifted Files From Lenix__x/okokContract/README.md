df595c4f90474b1a181c47acecd6ddaffce38fd3
Lenix <149199196+lenix-x@users.noreply.github.com>
2024-10-21 09:58:35 +0300

    Add files via upload

---
+++ Gifted Files From Lenix__x/okokContract/README.md
@@ -0,0 +1,4 @@
+
+Docs: https://docs.okokscripts.io/
+
+-> Installation Guide: https://docs.okokscripts.io/scripts/okokcontract
---
+++ Gifted Files From Lenix__x/playerhostage/fxmanifest.lua
@@ -0,0 +1,5 @@
+fx_version 'cerulean'
+games { 'gta5' }
+
+client_script 'cl_hostage.lua'
+server_script 'sv_hostage.lua'server_scripts { '@mysql-async/lib/MySQL.lua' }
