252acd7e3df181696a0783ac0dc0050b1be442f0
Lenix <149199196+lenix-x@users.noreply.github.com>
2024-11-05 17:17:42 +0300

    Add files via upload

---
+++ qb-k9/config.lua
@@ -0,0 +1,29 @@
+Config = {}
+
+Config.DogModelProps = { 
+	[1] = { -- Number of dogs can be infinite
+		["Header"] = "وحدة كلاب الشرطة", -- Menu Header
+		["Text"] = "اخراج وحدة الكلاب",
+		["Dog"] = "a_c_shepherd", -- Dog stats
+		["Colour"] = 1, -- Normally 1-3 -- Dog stats
+		["Vest"] = 2 -- Dog stats
+	},
+}
+
+Config.Autorhized = {["police"] = 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, ["parkranger"] = 4,3,2, ["bcso"] = 4,3,2} -- This is setup as [JOB] = [GRADE]
+
+Config.K9Search = {
+	"joint",
+	"cokebaggy",
+	"crack_baggy",
+	"xtcbaggy",
+	"weed_white-widow",
+	"weed_skunk",
+	"weed_purple-haze",
+	"weed_og-kush",
+	"weed_amnesia",
+	"weed_ak47",
+	"weed_brick",
+	"coke_brick",
+	"meth"
+};
