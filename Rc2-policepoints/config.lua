3941cf594315e27c969e81fdbf77e80d662dbbee
Lenix <149199196+lenix-x@users.noreply.github.com>
2024-12-06 20:53:40 +0300

    Delete Rc2-policepoints directory

--- Rc2-policepoints/config.lua
+++
@@ -1,8 +0,0 @@
-
-
-
-Rc2store = {}
-Rc2store.Minutes = 50 --Duration for earning police points: expressed in minutes.
-Rc2store.Points = 1   --how much points does the player get
-Rc2store.Core = 'qb-core'
-Rc2store.CheckPointsCommand = 'policepoints'
