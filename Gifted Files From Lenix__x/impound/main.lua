db69e6d4301b6c0c2edae9360299b5f4b8e7f6b4
Lenix <149199196+lenix-x@users.noreply.github.com>
2024-12-25 20:09:28 +0300

    Update main.lua

--- Gifted Files From Lenix__x/impound/main.lua
+++ Gifted Files From Lenix__x/impound/main.lua
@@ -1,13 +1,13 @@
 local QBCore = exports['qb-core']:GetCoreObject()
 
-RegisterServerEvent('dynyx-impound:server:VehicS', function()
+RegisterServerEvent('impound:server:VehicS', function()
     local src = source
     local Player = QBCore.Functions.GetPlayer(src)
     TriggerClientEvent('QBCore:Command:DeleteVehicle', src)
     TriggerEvent('qb-log:server:CreateLog', 'default', 'تم حجزها بنجاح', "lightgreen", (Player.PlayerData.charinfo.firstname .. ' ' .. Player.PlayerData.charinfo.lastname).. " has scuff impounded a vehicle.")
 end)
 
-RegisterServerEvent('dynyx-impound:server:ParkingVio', function()
+RegisterServerEvent('impound:server:ParkingVio', function()
     local src = source
     local Player = QBCore.Functions.GetPlayer(src)
     if Player.PlayerData.job.name == "police" then
@@ -19,7 +19,7 @@ RegisterServerEvent('dynyx-impound:server:ParkingVio', function()
     end
 end)
 
-RegisterServerEvent('dynyx-impound:server:PDIm', function()
+RegisterServerEvent('impound:server:PDIm', function()
     local src = source
     local Player = QBCore.Functions.GetPlayer(src)
     if Player.PlayerData.job.name == "police" then
