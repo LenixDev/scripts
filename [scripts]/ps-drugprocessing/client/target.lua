3a1b920c9648cc0570b8d1c429bb16a09ccf9f4b
Lenix <149199196+lenix-x@users.noreply.github.com>
2025-05-10 13:03:54 +0100

    Add files via upload

---
+++ [scripts]/ps-drugprocessing/client/target.lua
@@ -0,0 +1,448 @@
+local QBCore = exports['qb-core']:GetCoreObject()
+
+CreateThread(function()
+    local peds = {'a_m_m_hillbilly_02','a_m_m_mlcrisis_01','mp_f_weed_01',}
+    for k, v in pairs ( peds) do 
+        RequestModel(v)
+        while not HasModelLoaded(v) do
+            Wait(100)
+        end
+    end
+    local walterloc = vector4(-1187.73, -445.27, 43.91-1, 289.45)
+    local walter = CreatePed(0, 'a_m_m_hillbilly_02',walterloc.x, walterloc.y, walterloc.z, walterloc.w, false, false)
+    FreezeEntityPosition(walter, true)
+    SetEntityInvincible(walter, true)
+    SetBlockingOfNonTemporaryEvents(walter, true)
+    local options = {
+        {
+			type = "client",
+			event = "ps-drugprocessing:EnterLab",
+			icon = "fas fa-atom",
+            distance = 2.0,
+			label = Lang:t("target.talk_to_walter"),
+        }
+    }
+    if Config.OxTarget then
+        exports.ox_target:addLocalEntity(walter, options)
+    else
+	    exports['qb-target']:AddTargetEntity(walter, { options = options, distance = 2.0})
+    end   
+    local dracoloc = vector4(812.49, -2399.59, 23.66-1, 223.1)
+    local draco = CreatePed(0, 'a_m_m_mlcrisis_01',dracoloc.x, dracoloc.y, dracoloc.z, dracoloc.w, false, false)
+    FreezeEntityPosition(draco, true)
+    SetEntityInvincible(draco, true)
+    SetBlockingOfNonTemporaryEvents(draco, true)
+    local options2 = {
+        {
+            type = "client",
+            event = "ps-drugprocessing:EnterCWarehouse",
+            icon = "fas fa-key",
+            label = Lang:t("target.talk_to_draco"),
+            distance = 2.0,
+        }
+    }
+    if Config.OxTarget then
+        exports.ox_target:addLocalEntity(draco, options2)
+    else
+	    exports['qb-target']:AddTargetEntity(draco, { options = options2, distance = 2.0})
+    end  
+    local charlotteloc = vector4(102.07, 175.08, 104.59-1, 159.91)
+    local charlotte = CreatePed(0, 'mp_f_weed_01',charlotteloc.x, charlotteloc.y, charlotteloc.z, charlotteloc.w, false, false)
+    FreezeEntityPosition(charlotte, true)
+    SetEntityInvincible(charlotte, true)
+    SetBlockingOfNonTemporaryEvents(charlotte, true)
+    local options3 = {
+        {
+            type = "client",
+            event = "ps-drugprocessing:EnterWWarehouse",
+            icon = "fas fa-key",
+            label = Lang:t("target.talk_to_charlotte"),
+            distance = 2.0,
+        }
+    }
+    if Config.OxTarget then
+        exports.ox_target:addLocalEntity(charlotte, options3)
+    else
+	    exports['qb-target']:AddTargetEntity(charlotte, { options = options3, distance = 2.0})
+    end
+end)
+
+CreateThread(function()
+    exports["qb-target"]:AddBoxZone("chemmenu", vector3(3535.66, 3661.69, 28.12), 1.65, 2.4, {
+        name = "chemmenu",
+        heading = 350.0,
+        debugPoly = false,
+        minZ = 27.52,
+        maxZ = 29.12,
+    }, {
+        options = {
+            {
+                type = "client",
+                event = "ps-drugprocessing:chemicalmenu",
+                icon = "fas fa-vials",
+                label = Lang:t("target.chemmenu"),
+                --job = "pawnshop", -- Remove this line if you do not want a job check.
+            },
+        },
+    distance = 2.5
+    })
+    exports["qb-target"]:AddBoxZone("methprocess", vector3(978.22, -147.1, -48.53), 1.6, 1.8, {
+        name = "methprocess",
+        heading = 0,
+        debugPoly = false,
+        minZ = -50.33,
+        maxZ = -45.53,
+    }, {
+        options = {
+            {
+                type = "client",
+                event = "ps-drugprocessing:ProcessChemicals",
+                icon = "fas fa-vials",
+                label = Lang:t("target.methprocess"),
+                --job = "methcook", -- Remove this line if you do not want a job check.
+            },
+        },
+    distance = 3.5
+    })
+    exports["qb-target"]:AddBoxZone("methtempup", vector3(982.56, -145.59, -49.0), 1.2, 1.4, {
+        name = "methtempup",
+        heading = 0,
+        debugPoly = false,
+        minZ = -50.3,
+        maxZ = -47.3,
+    }, {
+        options = {
+            {
+                type = "client",
+                event = "ps-drugprocessing:ChangeTemp",
+                icon = "fas fa-temperature-empty",
+                label = Lang:t("target.methtempup"),
+                --job = "methcook", -- Remove this line if you do not want a job check.
+            },
+        },
+    distance = 3.5
+    })
+    exports["qb-target"]:AddBoxZone("methtempdown", vector3(979.59, -144.14, -49.0), 1.2, 0.5, {
+        name = "methtempdown",
+        heading = 354,
+        debugPoly = false,
+        minZ = -49.2,
+        maxZ = -47.9,
+    }, {
+        options = {
+            {
+                type = "client",
+                event = "ps-drugprocessing:ChangeTemp2",
+                icon = "fas fa-temperature-full",
+                label = Lang:t("target.methtempdown"),
+                --job = "methcook", -- Remove this line if you do not want a job check.
+            },
+        },
+    distance = 3.5
+    })
+    exports["qb-target"]:AddBoxZone("methbagging", vector3(987.44, -140.5, -49.0), 0.5, 0.7, {
+        name = "methbagging",
+        heading = 1,
+        debugPoly = false,
+        minZ = -49.35,
+        maxZ = -48.65,
+    }, {
+        options = {
+            {
+                type = "client",
+                event = "ps-drugprocessing:ProcessProduct",
+                icon = "fas fa-box",
+                label = Lang:t("target.bagging"),
+                --job = "methcook", -- Remove this line if you do not want a job check.
+            },
+        },
+    distance = 3.5
+    })
+    exports["qb-target"]:AddBoxZone("methkeypad", vector3(969.04, -146.17, -46.4), 0.4, 0.2, {
+        name = "methkeypad",
+        heading = 0,
+        debugPoly = false,
+        minZ = -46.2,
+        maxZ = -45.8,
+    }, {
+        options = {
+            {
+                type = "client",
+                event = "ps-drugprocessing:ExitLab",
+                icon = "fas fa-lock",
+                label = Lang:t("target.keypad"),
+                --job = "methcook", -- Remove this line if you do not want a job check.
+            },
+        },
+    distance = 3.5
+    })
+    exports["qb-target"]:AddBoxZone("cokeleave", vector3(1088.56, -3187.02, -38.67), 1, 1, {
+        name = "cokeleave",
+        heading = 0,
+        debugPoly = false,
+        minZ = -38.74,
+        maxZ = -38.54,
+    }, {
+        options = {
+            {
+                type = "client",
+                event = "ps-drugprocessing:ExitCWarehouse",
+                icon = "fas fa-lock",
+                label = Lang:t("target.keypad"),
+                --job = "cokecutter", -- Remove this line if you do not want a job check.
+            },
+        },
+    distance = 3.5
+    })
+    exports["qb-target"]:AddBoxZone("cokeleafproc", vector3(1086.2, -3194.9, -38.99), 2.5, 1.4, {
+        name = "cokeleafproc",
+        heading = 0,
+        debugPoly = false,
+        minZ = -39.39,
+        maxZ = -38.39,
+    }, {
+        options = {
+            {
+                type = "client",
+                event = "ps-drugprocessing:ProcessCocaFarm",
+                icon = "fas fa-scissors",
+                label = Lang:t("target.cokeleafproc"),
+                --job = "cokecutter", -- Remove this line if you do not want a job check.
+            },
+        },
+    distance = 3.5
+    })
+    exports["qb-target"]:AddBoxZone("cokepowdercut", vector3(1092.89, -3195.78, -38.99), 7.65, 1.2, {
+        name = "cokepowdercut",
+        heading = 90,
+        debugPoly = false,
+        minZ = -39.39,
+        maxZ = -38.44,
+    }, {
+        options = {
+            {
+                type = "client",
+                event = "ps-drugprocessing:ProcessCocaPowder",
+                icon = "fas fa-weight-scale",
+                label = Lang:t("target.cokepowdercut"),
+                --job = "cokecutter", -- Remove this line if you do not want a job check.
+            },
+        },
+    distance = 3.5
+    })
+    exports["qb-target"]:AddBoxZone("cokebricked", vector3(1100.51, -3199.46, -38.93), 2.6, 1.0, {
+        name = "cokebricked",
+        heading = 90,
+        debugPoly = false,
+        minZ = -39.99,
+        maxZ = -38.59,
+    }, {
+        options = {
+            {
+                type = "client",
+                event = "ps-drugprocessing:ProcessBricks",
+                icon = "fas fa-weight-scale",
+                label = Lang:t("target.bagging"),
+                --job = "cokecutter", -- Remove this line if you do not want a job check.
+            },
+        },
+    distance = 3.5
+    })
+    exports["qb-target"]:AddBoxZone("weedproces", vector3(1038.37, -3206.06, -38.17), 2.6, 1.0, {
+        name = "weedproces",
+        heading = 0,
+        debugPoly = false,
+        minZ = -38.37,
+        maxZ = -37.57,
+    }, {
+        options = {
+            {
+                type = "client",
+                event = "ps-drugprocessing:processWeed",
+                icon = "fas fa-envira",
+                label = Lang:t("target.weedproces"),
+            },
+        },
+    distance = 3.5
+    })
+    exports["qb-target"]:AddBoxZone("weedkeypad", vector3(1066.51, -3183.44, -39.16), 1.6, 0.4, {
+        name = "weedkeypad",
+        heading = 0,
+        debugPoly = false,
+        minZ = -40.16,
+        maxZ = -37.76,
+    }, {
+        options = {
+            {
+                type = "client",
+                event = "ps-drugprocessing:ExitWWarehouse",
+                icon = "fas fa-lock",
+                label = Lang:t("target.keypad"),
+                --job = "weedcutter", -- Remove this line if you do not want a job check.
+            },
+        },
+    distance = 3.5
+    })
+    exports["qb-target"]:AddBoxZone("heroinproces", vector3(1384.9, -2080.61, 52.21), 2.5, 2.5, {
+        name = "heroinproces",
+        heading = 223.98,
+        debugPoly = false,
+        minZ = 51.21,
+        maxZ = 53.21,
+    }, {
+        options = {
+            {
+                type = "client",
+                event = "ps-drugprocessing:processHeroin",
+                icon = "fas fa-envira",
+                label = Lang:t("target.heroinproces"),
+            },
+        },
+    distance = 3.5
+    })
+    exports['qb-target']:AddBoxZone("thychloride", vector3(-679.77, 5800.7, 17.33), 1, 1, {
+		name="thychloride",
+		heading=340.0,
+		debugPoly=false,
+		minZ = 14.33,
+		maxZ = 18.33,
+	},{
+	   options = {
+			{
+				type = "client",
+				event = "ps-drugprocessing:processingThiChlo",
+				icon = "fas fa-biohazard",
+				label = Lang:t("target.process_thionyl_chloride"),
+			},
+		},
+	distance = 2.5
+	})
+    exports['qb-target']:AddBoxZone("heroinproc", vector3(1413.7, -2041.77, 52.0), 1, 1, {
+		name="heroinproc",
+		heading=352.15,
+		debugPoly=false,
+		minZ = 51.00,
+		maxZ = 53.00,
+	},{
+	    options = {
+			{
+				type = "client",
+				event = "ps-drugprocessing:ProcessPoppy",
+				icon = "fas fa-leaf",
+				label = Lang:t("target.heroinproc"),
+			},
+			},
+	distance = 2.5
+	})
+end)
+
+CreateThread(function()
+    exports['qb-target']:AddTargetModel("h4_prop_bush_cocaplant_01", {
+        options = {
+            {
+                type = "client",
+                event = "ps-drugprocessing:pickCocaLeaves",
+                icon = "fas fa-leaf",
+                label = Lang:t("target.pickCocaLeaves"),
+            },
+        },
+        distance = 4.0
+    })
+    exports['qb-target']:AddTargetModel("prop_plant_01b", {
+        options = {
+            {
+                type = "client",
+                event = "ps-drugprocessing:pickHeroin",
+                icon = "fas fa-leaf",
+                label = Lang:t("target.pickHeroin"),
+            },
+        },
+        distance = 4.0
+    })
+    exports['qb-target']:AddTargetModel("mw_weed_plant", {
+        options = {
+            {
+                type = "client",
+                event = "ps-drugprocessing:pickWeed",
+                icon = "fas fa-envira",
+                label = Lang:t("target.pickWeed"),
+            },
+        },
+        distance = 4.0
+    })
+    exports['qb-target']:AddTargetModel("mw_sodium_barrel", {
+        options = {
+            {
+                type = "client",
+                event = "ps-drugprocessing:pickSodium",
+                icon = "fas fa-dna",
+                label = Lang:t("target.pickSodium"),
+            },
+        },
+        distance = 4.0
+    })
+    exports['qb-target']:AddTargetModel("mw_sulfuric_barrel", {
+        options = {
+            {
+                type = "client",
+                event = "ps-drugprocessing:pickSulfuric",
+                icon = "fas fa-shield-virus",
+                label = Lang:t("target.pickSulfuric"),
+            },
+        },
+        distance = 4.0
+    })
+    exports['qb-target']:AddTargetModel("mw_chemical_barrel", {
+        options = {
+            {
+                type = "client",
+                event = "ps-drugprocessing:pickChemicals",
+                icon = "fas fa-radiation",
+                label = Lang:t("target.pickChemicals"),
+            },
+        },
+        distance = 4.0
+    })
+    exports['qb-target']:AddTargetModel("mw_hydro_barrel", {
+        options = {
+            {
+                type = "client",
+                event = "ps-drugprocessing:client:hydrochloricacid",
+                icon = "fas fa-radiation",
+                label = Lang:t("target.hydrochloricacid"),
+            },
+        },
+        distance = 4.0
+    })
+end)
+
+Citizen.CreateThread(function()
+    local blipsToCreate = {
+        {coords = vector3(2224.64, 5577.03, 53.85)},
+        {coords = vector3(-1069.25, 4945.57, 212.18)},
+        {coords = vector3(-3026.89, 3334.91, 10.04)},
+        {coords = vector3(-389.35, -1874.85, 20.53)},
+        {coords = vector3(1264.97, 1803.96, 82.94)},
+        {coords = vector3(2806.5, 4774.46, 46.98)},
+        {coords = vector3(-2339.15, -54.32, 95.05)},
+        {coords = vector3(1413.37, -2041.74, 52.0)},
+        {coords = vector3(2503.84, -428.11, 92.99)},
+        {coords = vector3(-679.59, 5800.46, 17.33)},
+        {coords = vector3(-1187.17, -446.24, 43.91)},
+        {coords = vector3(813.21, -2398.69, 23.66)},
+        {coords = vector3(102.07, 175.09, 104.59)},
+        {coords = vector3(3536.71, 3662.63, 28.12)},
+    }
+
+    for _, blipInfo in ipairs(blipsToCreate) do
+        local blip = AddBlipForCoord(blipInfo.coords.x, blipInfo.coords.y, blipInfo.coords.z) -- Create blip at specified coordinates
+        SetBlipSprite(blip, 310)
+        SetBlipColour(blip, 1)
+        SetBlipScale(blip, 0.4)
+        SetBlipAsShortRange(blip, true)
+
+        BeginTextCommandSetBlipName("STRING")
+        AddTextComponentSubstringPlayerName('Anonymous')
+        EndTextCommandSetBlipName(blip)
+    end
+end)
